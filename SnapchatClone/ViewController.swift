30596af35f2133ce694aa5375dd29fcc8fab291e
Tolga Kağan <tolga.kagan@hotmail.com>
2021-05-02 13:45:49 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Tolga Kağan Aysu on 2.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
